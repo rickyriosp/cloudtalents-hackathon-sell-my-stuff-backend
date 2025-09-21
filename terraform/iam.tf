79a39024ce77ccde283361b3a31231614640d978
rickyriosp <riki.rios91@gmail.com>
2025-09-21 15:44:06 -0400

    chore: rename IAM role to 'sellmystuff_lambda' and add Bedrock and CloudWatch access policies

---
+++ terraform/iam.tf
@@ -0,0 +1,44 @@
+# IAM policy for Bedrock access
+data "aws_iam_policy_document" "bedrock_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "bedrock:InvokeModel",
+      "bedrock:InvokeModelWithResponseStream",
+      "bedrock:ListFoundationModels"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "bedrock_access" {
+  name   = "lambda_bedrock_access"
+  policy = data.aws_iam_policy_document.bedrock_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_bedrock" {
+  role       = aws_iam_role.sellmystuff_lambda.name
+  policy_arn = aws_iam_policy.bedrock_access.arn
+}
+
+data "aws_iam_policy_document" "cloudwatch_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "cloudwatch:PutMetricData",
+      "cloudwatch:GetMetricData",
+      "cloudwatch:ListMetrics"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "cloudwatch_access" {
+  name   = "lambda_cloudwatch_access"
+  policy = data.aws_iam_policy_document.cloudwatch_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_cloudwatch" {
+  role       = aws_iam_role.sellmystuff_lambda.name
+  policy_arn = aws_iam_policy.cloudwatch_access.arn
+}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-resource "aws_iam_role" "example" {
+resource "aws_iam_role" "sellmystuff_lambda" {
   name               = "lambda_execution_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
